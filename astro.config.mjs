0ec95872d42dd51b914c94cba39994826ee3612e
crfdiab <trickyaffiliate.com@gmail.com>
2025-12-11 12:42:59 +0200

    Done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://crfdiab.github.io',
+  base: '/athek-deals',
   vite: {
     plugins: [tailwindcss()]
   }
